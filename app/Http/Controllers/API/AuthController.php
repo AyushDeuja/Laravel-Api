1492407fd683adefc37c2c24f35677ffe3f53ff0
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-29 15:01:09 +0545

    login api done

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -38,6 +39,35 @@ class AuthController extends Controller
             ],200);
     }
     public function login(Request $request) {
+         $validateUser = Validator::make(
+            $request->all(),
+            [
+                'email'=>'required|email',
+                'password'=> 'required',
+            ]
+            );
+             if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Authentication failed',
+                    'errors' => $validateUser->errors()->all()
+                ],404);
+
+            }
+            if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+                $authUser = Auth::user();
+                return response()->json([
+                'status' => true,
+                'message' => 'User Logged in Successfully',
+                'token' => $authUser->createToken("API Token")->plainTextToken,
+                'token_type' => 'bearer'
+            ],200);
+            }else{
+                return response()->json([
+                'status' => false,
+                'message' => 'Email and password not matched',
+            ],401);
+            }
 
     }
     public function logout(Request $request){}
